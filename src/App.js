d8cefb11dff17fc0c32299a2445bb28389d3fd3c
Kielbik <kielbaldo@gmail.com>
2023-08-31 14:12:19 +0100

    search input

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     }
+    console.log("constructor")
   }
 
   componentDidMount() {
+    console.log("componentDidMount")
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -27,8 +29,24 @@ class App extends Component {
   }
 
   render() {
+    console.log("render")
     return (
-      <div className="App">
+      <div className='App'>
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={(event) => {
+            const searchString = event.target.value.toLocaleLowerCase()
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString)
+            })
+
+            this.setState(() => {
+              return { monsters: filteredMonsters }
+            })
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
